b1c00b01459c9bb0a429ecc6e8ff1dffea0408d4
itsmurugappan <itsmurugappan@gmail.com>
2020-09-23 18:54:05 -0700

    actions

--- function/template/go/main.go
+++ function/template/go/main.go
@@ -2,6 +2,8 @@
 // Copyright (c) OpenFaaS Author(s) 2018. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+// +build exclude
+
 package main
 
 import (
